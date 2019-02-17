d61289a94093754f08f74f20f7918c38e4b0392e
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2019-02-17 20:19:18 +0300

    Add files via upload

--- case9.py
+++ case9.py
@@ -3,11 +3,29 @@ import random
 start = {'bitcoins': 10000, 'staff': 100, 'territory': 150, 'gigabytes': 10000, 'working conditions': 5, 'quarter': 0}
 
 
+def selling():
+    """Information about selling"""
+    price1 = random.randint(20, 35)
+    buyers = 'Люди готовы купить 1 Гб информации за', price1, 'биткойнов. Сколько Гб продать?'
+    print(*buyers)
+    amount1 = int(input())
+    _max1 = start.get('gigabytes', [])
+    update2 = {'bitcoins': start.get('bitcoins', []) + amount1 * price1,
+               'gigabytes': start.get('gigabytes', []) - amount1}
+    update3 = {'bitcoins': start.get('bitcoins', []) + _max1 * price1,
+               'gigabytes': start.get('gigabytes', []) - _max1}
+    if amount1 < start.get('gigabytes', []):
+        start.update(update2)
+    else:
+        print('Вы не можете столько продать, максимум:', _max1)
+        start.update(update3)
+
+
 def buying():
     """Information about buying"""
     price = random.randint(20, 35)
-    seller = 'На сегодняшний день 1 Гб информации на черном рынке продается за', price, 'биткойнов. Сколько гб купить?'
-    print(*seller)
+    sellers = 'На сегодняшний день 1 Гб информации на флешках продается за', price, 'биткойнов. Сколько Гб купить?'
+    print(*sellers)
     amount = int(input())
     _max = start.get('gigabytes', []) // price
     update = {'bitcoins': start.get('bitcoins', []) - amount * price, 'gigabytes': start.get('gigabytes', []) + amount}
@@ -15,12 +33,10 @@ def buying():
     if amount * price < start.get('gigabytes', []):
         start.update(update)
     else:
-        print('Вы не можете купить столько, максимум:', _max)
+        print('Вы не можете столько купить, максимум:', _max)
         start.update(update1)
 
 
-
-
-
+selling()
 buying()
 print(start)
