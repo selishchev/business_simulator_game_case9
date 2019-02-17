14057ff9fad3edc04afd9b3b4864b0733777b456
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2019-02-17 19:44:35 +0300

    Add files via upload

--- case9.py
+++ case9.py
@@ -1,1 +1,26 @@
-start = {'grain': 10000, 'people': 100, 'territory': 150, 'money': 10000, 'distemper': 5, 'year': 0}
+import random
+
+start = {'bitcoins': 10000, 'staff': 100, 'territory': 150, 'gigabytes': 10000, 'working conditions': 5, 'quarter': 0}
+
+
+def buying():
+    """Information about buying"""
+    price = random.randint(20, 35)
+    seller = 'На сегодняшний день 1 Гб информации на черном рынке продается за', price, 'биткойнов. Сколько гб купить?'
+    print(*seller)
+    amount = int(input())
+    _max = start.get('gigabytes', []) // price
+    update = {'bitcoins': start.get('bitcoins', []) - amount * price, 'gigabytes': start.get('gigabytes', []) + amount}
+    update1 = {'bitcoins': start.get('bitcoins', []) - _max * price, 'gigabytes': start.get('gigabytes', []) + _max}
+    if amount * price < start.get('gigabytes', []):
+        start.update(update)
+    else:
+        print('Вы не можете купить столько, максимум:', _max)
+        start.update(update1)
+
+
+
+
+
+buying()
+print(start)
